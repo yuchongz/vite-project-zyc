b4843cf32c2447bc55589a114c3a489c0c6317c2
zhengyuchong <yuchongzgd@outlook.com>
2026-05-20 17:11:49 +0800

    feat: unplugin-vue-components unplugin-auto-import

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>Hello world unplugin-vue-router</div>
-  <RouterLink to="/about">About</RouterLink>
-  <RouterLink to="/">Home</RouterLink>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div ref="target">
+    <div>Hello world unplugin-vue-router</div>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/">Home</RouterLink>
+    <div>{{ msg }}</div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <p>x: {{ x }}</p>
+    <p>y: {{ y }}</p>
+    <p>isOutside: {{ isOutside }}</p>
+    <HelloWorld></HelloWorld>
+    <UserComponent></UserComponent>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+const msg = ref('Hello unplugin-auto-import')
+const target = useTemplateRef('target')
+
+const { x, y, isOutside } = useMouseInElement(target)
 </script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,40 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueJsx(), vueDevTools(), UnoCSS()],
+  plugins: [
+    VueRouter(),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        // 'vue-router',
+        VueRouterAutoImports,
+        '@vueuse/core',
+      ],
+    }),
+    Components({
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
